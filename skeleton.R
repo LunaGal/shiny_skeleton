1b7cc700961a89ce0f435e27f83c518f69073541
Van Wishingrad <vwishingrad@users.noreply.github.com>
2023-12-15 09:53:22 -1000

    Add files via upload
    fixed the issue with colourCount by using the correct data frame (test_table instead of zotuBar3). I also replaced zotuBar3 with test_table in the color palette function. minor formatting changes

--- skeleton.R
+++ skeleton.R
@@ -1,7 +1,6 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
-library(vegan)
 library(plyr)
 library(reshape2)
 library(data.table)
@@ -17,29 +16,28 @@ library(lsmeans)
 # Takes a taxonomy table
 # Outputs a taxonomy table that groups by `group` and counts the number of
 # observations of each species/genus/family/etc
-relabun_by_group <- function(table_input, group="Marker", taxlevel="species") {
+relabun_by_group <- function(table_input, group = "Marker", taxlevel = "species") {
   zotuBar <- table_input
-  zotuBar$Rep<-rep(1,nrow(zotuBar))
-
+  zotuBar$Rep <- rep(1, nrow(zotuBar))
+  
   # Group and count
-  zotuBar2<-ddply(zotuBar, c(taxlevel, group), summarize, Sum = sum(c(Rep)) )
-  # Count total number of each marker
-  zotuBarTotal<-aggregate(formula(paste0("Sum~",group)), data = zotuBar2, FUN = sum)
-  colnames(zotuBarTotal)[2]<-"Total"
-  zotuBar3<-merge(zotuBar2, zotuBarTotal, by = group)
-  zotuBar3$RelAbun<-zotuBar3$Sum/zotuBar3$Total
-
-  ##
-  zotuBar3$Quantity<-factor(zotuBar3[[group]])
-
-  # Filter out rows with blank in the "species" column
+  zotuBar2 <- ddply(zotuBar, c(taxlevel, group), summarize, Sum = sum(c(Rep)))
+  # Count the total number of each marker
+  zotuBarTotal <- aggregate(formula(paste0("Sum ~ ", group)), data = zotuBar2, FUN = sum)
+  colnames(zotuBarTotal)[2] <- "Total"
+  zotuBar3 <- merge(zotuBar2, zotuBarTotal, by = group)
+  zotuBar3$RelAbun <- zotuBar3$Sum / zotuBar3$Total
+  
+  zotuBar3$Quantity <- factor(zotuBar3[[group]])
+  
+  # Filter out rows with blanks in the "species" column
   zotuBar3_filtered <- zotuBar3[!is.na(zotuBar3$species) & zotuBar3$species != "", ]
   return(zotuBar3_filtered)
 }
 
 # Takes a table
 # Returns a ggplot code block
-make_plot <- function(in_table, taxlevel="species") {
+make_plot <- function(in_table, taxlevel = "species") {
   taxlevel <- sym(taxlevel)
   return({
     ggplot(in_table, aes(x = Quantity, y = Sum, fill = species)) +
@@ -60,10 +58,11 @@ make_plot <- function(in_table, taxlevel="species") {
         axis.line.y = element_line(color = "black", size = 0.5),
         legend.key.size = unit(1.2, "lines"),  # Adjust the legend key size here
         legend.text = element_text(size = 14)   # Adjust the legend text size here
-      )})
+      )
+  })
 }
 
-full_pipeline <- function(tax_table, group="Marker", taxlevel="species") {
+full_pipeline <- function(tax_table, group = "Marker", taxlevel = "species") {
   tab <- relabun_by_group(tax_table, group = group, taxlevel = taxlevel)
   return(make_plot(tab, taxlevel = taxlevel))
 }
@@ -72,10 +71,10 @@ full_pipeline <- function(tax_table, group="Marker", taxlevel="species") {
 
 phyl_levs <- c("kingdom", "phylum", "class", "order", "family", "genus", "species")
 group_options <- c("Marker", "Site")
-raw_table <- read.csv("Van_QIIME/Fishes.df4.csv")
+raw_table <- read.csv("Van_eDNAFlow/Fishes.df4.csv")
 test_table <- relabun_by_group(raw_table)
 getPalette <- colorRampPalette(brewer.pal(12, "Paired"))
-colourCount <- length(unique(zotuBar3$species))
+colourCount <- length(unique(test_table$species))
 
 # The app
 ui <- fluidPage(
@@ -83,7 +82,9 @@ ui <- fluidPage(
   selectInput("grouping", "Group By", choices = group_options),
   plotOutput("chart")
 )
+
 server <- function(input, output, session) {
-  output$chart <- renderPlot(full_pipeline(test_table, taxlevel=input$tax_level))
+  output$chart <- renderPlot(full_pipeline(test_table, taxlevel = input$tax_level))
 }
+
 shinyApp(ui, server)
